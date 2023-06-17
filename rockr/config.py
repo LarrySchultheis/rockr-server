72e477d7a4cce690bbe4f75c3c071d5b2f47046c
LarrySchultheis <43074882+LarrySchultheis@users.noreply.github.com>
2023-06-17 11:10:01 -0400

    AWS bb (#6)
    * Removed config file since we need to segregate secrety stuffs into local settings.py file
    * Setup db manager to connect to new AWS RDS instance

--- rockr/config.py
+++
@@ -1,6 +0,0 @@
-DATABSE_CONFIG = {
-    "host": "localhost",
-    "database": "rockr"
-}
-AUTH0_URL = "https://dev-6ary27eqnmjykel3.us.auth0.com/api/v2"
-TOKEN_URL = "https://dev-6ary27eqnmjykel3.us.auth0.com/oauth/token"
--- rockr/db/rockr_db_manager.py
+++ rockr/db/rockr_db_manager.py
@@ -1,16 +1,20 @@
 import psycopg2
-from rockr import config
+from rockr import settings
 
 # Simple starter class to wrap psycopg2 commands for DB access
 # In the future we should implement some ORM like SQLAlchemy
 # This will get us basic CRUD for now
 class DbManager():
     def __init__(self):
-        self.host = config.DATABSE_CONFIG['host']
-        self.db = config.DATABSE_CONFIG['database']
+        self.db = settings.DATABASE_CONFIG
 
     def _get_conn(self):
-        return psycopg2.connect(host=self.host, database=self.db)
+        return psycopg2.connect(host=self.db['host'], 
+                                port=self.db['port'],
+                                database=self.db['database'],
+                                user=self.db['user'],
+                                password=self.db['password']
+                                )
 
     def select(self, table, cols):
         query = f"SELECT {','.join(cols)} FROM {table}"
